a8626451de72d4f53af72e85a30a7ea967edd56d
Yerbol Syzdyk <erbcreedok@gmail.com>
2018-06-19 12:38:44 +0600

    waypoint disable

--- assets/js/functions/index-nav.js
+++ assets/js/functions/index-nav.js
@@ -4,12 +4,17 @@ $(document).ready(function() {
     var headerHeight = $('header').innerHeight() - 60;
 
     $('.navbar .main-nav li a').bind('click', function(e) {
-        e.preventDefault();
         var id = $(this).attr('href');
-        var elementOffset = document.querySelector(id).offsetTop;
-        $('html, body').animate({
-            scrollTop: elementOffset - headerHeight
-        }, '1000');
+        try {
+            var elementOffset = document.querySelector(id).offsetTop;
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: elementOffset - headerHeight
+            }, '1000');
+        } catch (DOMException) {
+            console.log('Ignoring selector');
+        }
+
     });
     var checkWaypoint = function( waypoint ) {
         var hash = '';
--- assets/js/main.js
+++ assets/js/main.js
@@ -14447,12 +14447,17 @@ $(document).ready(function() {
     var headerHeight = $('header').innerHeight() - 60;
 
     $('.navbar .main-nav li a').bind('click', function(e) {
-        e.preventDefault();
         var id = $(this).attr('href');
-        var elementOffset = document.querySelector(id).offsetTop;
-        $('html, body').animate({
-            scrollTop: elementOffset - headerHeight
-        }, '1000');
+        try {
+            var elementOffset = document.querySelector(id).offsetTop;
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: elementOffset - headerHeight
+            }, '1000');
+        } catch (DOMException) {
+            console.log('Ignoring selector');
+        }
+
     });
     var checkWaypoint = function( waypoint ) {
         var hash = '';
@@ -14731,6 +14736,4 @@ $(document).ready(function () {
         }, 3000);
     }
 });
-
-
 //# sourceMappingURL=main.js.map
